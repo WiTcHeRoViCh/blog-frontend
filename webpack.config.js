8d691f6f6c0cb9efcf9359061682f32c935f8be7
WiTcHeRoViCh <ua.bezprav@yandex.ua>
2018-02-22 12:04:21 +0200

    set up project configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,19 @@
 module.exports = {
-    entry: './src/app.js',
+    entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: './dist'
+    },
+    module: {
+        loaders: [
+            {
+                test: /.js?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react', 'stage-0'],
+                }
+            }
+        ]
     }
 };
