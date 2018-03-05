a3ea84cd1e6e051961acf1a10769e1bfcb5b894e
WiTcHeRoViCh <ua.bezprav@yandex.ua>
2018-03-05 15:37:01 +0200

    edit RenderUserPosts code

--- src/screens/PostsScreen/components/RenderUserPosts.js
+++ src/screens/PostsScreen/components/RenderUserPosts.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 
 import PostForm from './PostForm';
 
@@ -12,54 +11,56 @@ export default class extends Component {
         return user.hasOwnProperty('isOnline');
     };
 
+
+    renderUserPost = post => (
+        <div key={post._id}>
+            <h3>{post.title}</h3>
+
+            <div>{post.text}</div>
+            <img src={post.photoUrl} />
+
+            { this.isCurrentUser() ?
+                <div>
+                    <button id={post._id} onClick={this.props.handleDelete}>Delete</button>
+                    <button id={post._id} onClick={this.props.handleEditPost}>Edit</button>
+                </div>
+                :
+                null
+            }
+        </div>
+    );
+
+    renderEditPostForm = post => (
+        <div key={post._id}>
+            <PostForm submitFunc={this.props.formEditPostSubmit} {...post} submitButtonText={'Edit post'} />
+            <button id={post._id} onClick={this.props.handleEditPost}>Cancel</button>
+        </div>
+    );
+
+    renderNewPostForm = () => (
+        <div id='newPostDiv'>
+            <button onClick={this.props.handleToggleDisplayPostForm}>New post</button>
+            { this.props.state.displayForm && <PostForm submitFunc={this.props.formNewPostSubmit} submitButtonText={'Create new post'} /> }
+        </div>
+    );
+
+
     render(){
         const {
             user,
-            url,
             state,
-            handleDelete,
-            handleEditPost,
-            formNewPostSubmit,
-            formEditPostSubmit,
-            handleToggleDisplayPostForm,
         } = this.props;
 
         return (
             <div>
                 <div>{user.username} posts:</div>
-                { this.isCurrentUser() &&
-                    <div id='newPostDiv'>
-                        <button onClick={handleToggleDisplayPostForm}>New post</button>
-                        { state.displayForm && <PostForm submitFunc={formNewPostSubmit} submitButtonText={'Create new post'} /> }
-                    </div>
-                }
+                {this.isCurrentUser() && this.renderNewPostForm()}
 
                 <div>
                     {user.posts.map( post =>
                         (state.displayEditForm && state.editedPostId === post._id) ?
-                            (
-                                <div key={post._id}>
-                                    <PostForm submitFunc={formEditPostSubmit} {...post} submitButtonText={'Edit post'} />
-                                    <button id={post._id} onClick={handleEditPost}>Cancel</button>
-                                </div>
-                            )
-                            :
-                            (
-                                <div key={post._id}>
-                                    <h3>{post.title}</h3>
-                                    <div>{post.text}</div>
-                                    <img src={post.photoUrl} />
-
-                                    { this.isCurrentUser() ?
-                                        <div>
-                                            <button id={post._id} onClick={handleDelete}>Delete</button>
-                                            <button id={post._id} onClick={handleEditPost}>Edit</button>
-                                        </div>
-                                        :
-                                        null
-                                    }
-                                </div>
-                            )
+                            this.renderEditPostForm(post) :
+                            this.renderUserPost(post)
                     )}
                 </div>
             </div>
